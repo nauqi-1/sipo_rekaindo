83b158b798a9605a0481287a2f9db4891518e3c7
elinakma <elinakma005@gmail.com>
2025-01-08 09:24:45 +0700

    push

--- resources/views/components/confirm-success.blade.php
+++ resources/views/components/confirm-success.blade.php
@@ -10,15 +10,17 @@
   <div class="container">
     <div class="card">
       <div class="back-button">
-        <a href=""><</a>
+        <a href="{{ route('new-pw') }}"><</a>
       </div>
       <div class="logo">
         <img src="/img/logo-reka.png" alt="Reka Inka Group">
       </div>
       <img src="/img/success.png" alt="success">
       <h1 class="success">Successfully</h1>
-      <p>Your password has been reset successfully</p>
-      <button type="submit">UPDATE PASSWORD</button>
+      <p class="success_pw">Your password has been reset successfully</p>
+      <form action="#">
+        <button type="submit">CONTINUE</button>
+      </form>
     </div>
   </div>
 </body>
