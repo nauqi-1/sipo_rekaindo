59bccb440bca6d34d5fdcf7c205c9dee18893699
elinakma <elinakma005@gmail.com>
2025-01-08 11:13:49 +0700

    push

--- resources/views/components/new-pw.blade.php
+++ resources/views/components/new-pw.blade.php
@@ -10,7 +10,7 @@
   <div class="container">
     <div class="card">
       <div class="back-button">
-        <a href="#"><</a>
+        <a href="{{ route('verif-email') }}"><</a>
       </div>
       <div class="logo">
         <img src="/img/logo-reka.png" alt="Reka Inka Group">
@@ -21,7 +21,7 @@
       <form action="{{ route('confirm-success') }}" method="GET">
         <label for="new_pw">Enter New Password</label>
         <input type="password" id="new_pw" name="new-pw" placeholder="Enter new password" required>
-        <label for="confirm_pw">Enter New Password</label>
+        <label for="confirm_pw">Confirm Password</label>
         <input type="password" id="confirm_pw" name="confirm_pw" placeholder="Confirm password" required>
         <button type="submit">UPDATE PASSWORD</button>
       </form>
