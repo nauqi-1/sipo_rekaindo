17ad0480738e67cb65186daab8e61022692f553f
MSI\Hikmal <rezaardyansyah0@gmail.com>
2025-01-17 15:22:59 +0700

    fix

--- resources/views/superadmin/user-manage.blade.php
+++ resources/views/superadmin/user-manage.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'User Management')
       
 @section('content')
-    <div class="container">
+<div class="container">
         <div class="header">
             <!-- Back Button -->
             <div class="back-button">
@@ -12,24 +12,30 @@
             <h1>User Management</h1>
         </div>        
         <div class="row">
-            @include('components.breadcrumb')
+            <div class="breadcrumb-wrapper">
+                <div class="breadcrumb">
+                    <a href="#">Home</a> / <a href="#">Setting</a> / <a href="#">User Management</a>
+                </div>
+            </div>
         </div>
 
         <!-- Wrapper untuk elemen di luar card -->
         <div class="header-tools">
             <h2 class="title">User</h2>
             <div class="search-filter">
-                <button class="input-group-text" id="search-icon">
+            <form action="" method="GET" class="d-flex">
+                <button class="input-group-text" id="search-icon" name="search" type="submit">
                     <i class="bi bi-search"></i>
                 </button>
-                <input type="text" class="form-control" placeholder="Search by name...">
+                    <input type="text" name="search" id="search" class="form-control" placeholder="Search by name..." autocomplete="off">
+            </form>
                 <div class="dropdown">
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                         Filter by
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a class="dropdown-item" href="#">Aplhabetically A-Z</a></li>
-                        <li><a class="dropdown-item" href="#">Aplhabetically Z-A</a></li>
+                        <li><a class="dropdown-item" href="{{ route('user.manage', ['sort' => 'asc']) }}">Aplhabetically A-Z</a></li>
+                        <li><a class="dropdown-item" href="{{ route('user.manage', ['sort' => 'desc']) }}">Aplhabetically Z-A</a></li>
                     </ul>
                 </div>
                 <!-- Add User Button to Open Mod    al -->
@@ -52,44 +58,51 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>
-<<<<<<< HEAD
-                                <div class="user-info">
-                                    <img src="/img/user-manage/me1.jpg" alt="User Image" class="rounded-circle">
-                                    <div class="text-info">
-                                        <span>Fidela</span>
-                                        <br><small>fidel@gmail.com</small>
-                                    </div>
+                    @foreach ($users as $user)
+                    <tr>
+                        <td>
+                            <div class="user-info">
+                                <img src="/img/user-manage/me1.jpg" alt="User Image" class="rounded-circle">
+                                <div class="text-info">
+                                    <span>{{ $user->firstname }} {{ $user->lastname }}</span>
+                                    <br><small>{{ $user->email }}</small>
                                 </div>
-                            </td>
-                            <td><span class="badge bg-primary">Admin</span></td>
-                            <td>Keuangan</td>
-                            <td>Staff Keuangan</td>
-                            <td>081-222-123</td>
+                            </div>
+                        </td>
+                        <td>
+                            <span class="badge bg-primary">
+                             {{ $user->role->nm_role ?? 'No Role Assigned' }} <!-- Menampilkan nama role -->
+                            </span>
+                        </td>
+                        <td>
+                            {{ $user->divisi->nm_divisi ?? 'No Divisi Assigned' }} <!-- Menampilkan nama divisi -->
+                        </td>
+                        <td>
+                            {{ $user->position->nm_position ?? 'No Position Assigned' }} <!-- Menampilkan nama posisi -->
+                        </td>
+                        <td>{{ $user->phone_number }}</td>
                             <td>
-                                <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editUserModal">
-=======
                                 <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editUserModal" data-id="{{ $user->id }}" onclick="editUser({{ $user->id }})">
->>>>>>> 1f763ca9467873a118dc330996c1fb3e9afeeadd
                                     <img src="/img/user-manage/Edit.png" alt="edit">
                                 </button>
-                                <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal" data-id="{{ $user->id }}">
                                     <img src="/img/user-manage/Trash.png" alt="delete">
                                 </button>
                             </td>
                         </tr>
+                        @endforeach
                     </tbody>
                 </table>
-                <div class="pagination">
+                {{ $users->links('pagination::bootstrap-5') }}
+                <!-- <div class="pagination">
                     <p>Showing data 1 to 8 of 256K entries</p>
-                    <ul>
+                    <<ul>
                         <li class="active">1</li>
                         <li>2</li>
                         <li>3</li>
                         <li>...</li>
                     </ul>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
@@ -98,7 +111,7 @@
     <div class="modal fade" id="addUserModal" tabindex="-1" aria-labelledby="addUserModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
-                <form method="POST" action="">
+                <form method="POST" action="{{ route('user-manage/add') }}">
                     @csrf
                     <div class="modal-header">
                         <img src="/img/user-manage/addUser.png" alt="addUser" style="margin-right: 10px;">
@@ -108,22 +121,22 @@
                     <div class="modal-body">
                         <div class="row mb-3">
                             <div class="col-md-6">
-                                <label for="user_id" class="form-label">User ID :</label>
-                                <input type="text" name="user_id" id="user_id" class="form-control" required>
+                                <label for="id" class="form-label">User ID :</label>
+                                <input type="text" name="id" id="id" class="form-control" required autocomplete="id">
                             </div>
                             <div class="col-md-6">
-                                <label for="email_id" class="form-label">Email ID :</label>
-                                <input type="text" name="email_id" id="email_id" class="form-control" required>
+                                <label for="email" class="form-label">Email :</label>
+                                <input type="text" name="email" id="email" class="form-control" required autocomplete="email">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-6">
-                                <label for="first_name" class="form-label">First Name :</label>
-                                <input type="text" name="first_name" id="first_name" class="form-control" required autocomplete="firstname">
+                                <label for="firstname" class="form-label">First Name :</label>
+                                <input type="text" name="firstname" id="firstname" class="form-control" required autocomplete="firstname">
                             </div>
                             <div class="col-md-6">
-                                <label for="last_name" class="form-label">Last Name :</label>
-                                <input type="text" name="last_name" id="last_name" class="form-control" required autocomplete="lastname">
+                                <label for="lastname" class="form-label">Last Name :</label>
+                                <input type="text" name="lastname" id="lastname" class="form-control" required autocomplete="lastname">
                             </div>
                         </div>
                         <div class="row mb-3">
@@ -134,43 +147,53 @@
                             <div class="col-md-6">
                                 <label for="phone_number" class="form-label">Phone Number :</label>
                                 <input type="text" name="phone_number" id="phone_number" class="form-control" required autocomplete="phone_number">
+                                <x-input-error :messages="$errors->get('phone_number')" class="mt-2" />
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-6">
-                                <label for="password" class="form-label">Password :</label>
+                                <label for="password"  class="form-label">Password :</label>
                                 <input type="text" name="password" id="password" class="form-control" required autocomplete="new-password">
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
                             </div>
                             <div class="col-md-6">
-                                <label for="confirm_password" class="form-label">Confirm Password :</label>
-                                <input type="text" name="confirm_password" id="confirm_password" class="form-control" required autocomplete="new-password">
+                                <label for="password_confirmation" class="form-label">Confirm Password :</label>
+                                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" required autocomplete="new-password">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-6">
-                                <label for="divisi" class="form-label">Select Divisi</label>
-                                <select name="divisi" id="divisi" class="form-control">
+                                <label for="divisi_id_divisi" class="form-label">Select Divisi</label>
+                                <select name="divisi_id_divisi" id="divisi_id_divisi" class="form-control" required autofocus autocomplete="divisi_id_divisi">
+                                @foreach($divisi as $d)
+                                    <option value="{{ $d->id_divisi }}">{{ $d->nm_divisi }}</option>
+                                @endforeach
                                 </select>
                             </div>
                             <div class="col-md-6">
-                                <label for="position" class="form-label">Select Position</label>
-                                <select name="position" id="position" class="form-control">
-                                    <option value="Manager">Super Admin</option>
-                                    <option value="Supervisor">Supervisor/Manager</option>
-                                    <option value="Admin">Admin Divisi</option>
+                                <label for="position_id_position" class="form-label">Select Position</label>
+                                <select name="position_id_position" id="position_id_position" class="form-control" required autofocus autocomplete="position_id_position">
+                                @foreach($positions as $position)
+                                    <option value="{{ $position->id_position }}">{{ $position->nm_position }}</option>
+                                @endforeach
                                 </select>
                             </div>
                         </div>
                         <div class="mb-3">
-                            <label for="role" class="form-label">Module Permissions</label>
+                            <label for="role_id_role" class="form-label">Module Permissions</label>
                             <div>
+                            @foreach ($roles as $role)
+                                <input type="radio" name="role_id_role" value="{{ $role->id_role }}" id="role_{{ $role->id_role }}" required autofocus autocomplete="role_id_role">
+                                <label for="role_{{ $role->id_role }}">{{ $role->nm_role }}</label>
+                             @endforeach
                             </div>
                         </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                         <!-- <button type="submit" class="btn btn-primary">Add User</button> -->
-                        <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#successModal">Add User</button>
+                        <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="successModal">Add User</button>
+                        <!-- data-bs-toggle="modal" data-bs-target="#successModal" -->
                     </div>
                 </form>
             </div>
@@ -201,74 +224,6 @@
     <div class="modal fade" id="editUserModal" tabindex="-1" aria-labelledby="editUserModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
-<<<<<<< HEAD
-                <form method="POST" action="">
-                    @csrf
-                    <div class="modal-header">
-                        <img src="/img/user-manage/editUser.png" alt="addUser" style="margin-right: 10px;">
-                        <h5 class="modal-title" id="addUserModalLabel"><b>Edit User</b></h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="user_id" class="form-label">User ID :</label>
-                                <input type="text" name="user_id" id="user_id" class="form-control" required>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="email" class="form-label">Email :</label>
-                                <input type="text" name="email" id="email" class="form-control" required autofocus autocomplete="email">
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="firstname" class="form-label">First Name :</label>
-                                <input type="text" name="firstname" id="firstname" class="form-control" required autofocus autocomplete="firstname">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="lastname" class="form-label">Last Name :</label>
-                                <input type="text" name="lastname" id="lastname" class="form-control" required autofocus autocomplete="lastname">
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="username" class="form-label">Username :</label>
-                                <input type="text" name="username" id="username" class="form-control" required autofocus autocomplete="username">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="phone_number" class="form-label">Phone Number :</label>
-                                <input type="text" name="phone_number" id="phone_number" class="form-control" required autofocus autocomplete="phone_number">
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="password" class="form-label">Password :</label>
-                                <input type="text" name="password" id="password" class="form-control" required autocomplete="new-password">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="confirm_password" class="form-label">Confirm Password :</label>
-                                <input type="text" name="confirm_password" id="confirm_password" class="form-control" required autocomplete="new-password">
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="divisi" class="form-label">Select Divisi</label>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="position" class="form-label">Select Position</label>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="role" class="form-label">Module Permissions</label>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                        <!-- <button type="submit" class="btn btn-primary">Add User</button> -->
-                        <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editSuccessModal">Save changes</button>
-                    </div>
-                </form>
-=======
             <form method="POST" action="{{ route('user-manage/update', ['id' => $user->id]) }}">
     @csrf
     @method('PUT') <!-- Gunakan PUT method untuk update data -->
@@ -352,7 +307,6 @@
     </div>
 </form>
 
->>>>>>> 1f763ca9467873a118dc330996c1fb3e9afeeadd
             </div>
         </div>
     </div>
