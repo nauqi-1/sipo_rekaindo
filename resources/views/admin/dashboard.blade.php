769626c3ff9934c72b3d674e99817fb890f6d7af
elinakma <elinakma005@gmail.com>
2025-01-20 14:33:29 +0700

    push

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/supervisor/dashboard.css">
+</head>
+<body>
+    <div class="container">
+        <!-- Header -->
+        <header class="header">
+            <h1>Dashboard</h1>
+        </header>
+
+        <!-- Welcome Message -->
+        <div class="welcome-message">
+            <p>Selamat datang <strong>Mawar</strong> di <span class="system-name">Sistem Persuratan!</span> Anda login sebagai <span class="role-badge-admin">Supervisor</span></p>
+        </div>
+
+        <!-- Overview Section -->
+        <div class="overview-container">
+            <h3>Overview</h3>
+            <div class="overview-cards">
+                <div class="overview-card">
+                    <h4>MEMO</h4>
+                    <p><strong>4</strong> Memo</p>
+                </div>
+                <div class="overview-card">
+                    <h4>RISALAH RAPAT</h4>
+                    <p><strong>7</strong> Risalah Rapat</p>
+                </div>
+                <div class="overview-card">
+                    <h4>UNDANGAN RAPAT</h4>
+                    <p><strong>15</strong> Undangan Rapat</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
