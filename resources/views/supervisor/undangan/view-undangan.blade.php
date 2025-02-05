518045e6097e5c3710bb7eec0c71b0cdc91e78fa
elinakma <elinakma005@gmail.com>
2025-02-05 13:51:59 +0700

    p

---
+++ resources/views/supervisor/undangan/view-undangan.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Undangan Rapat</title>
+    <link href="https://cdn.jsdelivr.net/npm/summernote/dist/summernote-lite.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote/dist/summernote-lite.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/css/supervisor/view-undangan.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <!-- Back Button -->
+            <div class="back-button">
+                <a href="{{route ('memo.superadmin')}}"><img src="/img/user-manage/Vector_back.png" alt=""></a>
+            </div>
+            <h1>Undangan Rapat</h1>
+        </div>        
+        <div class="row">
+            <div class="breadcrumb-wrapper">
+                <div class="breadcrumb" style="gap: 5px;">
+                    <a href="#">Beranda</a>/<a href="#" style="color: #565656;">Undangan Rapat</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="row mb-4" style="gap: 20px;">
+                <div class="col">
+                    <div class="card-blue">
+                        <label for="tgl_surat" class="form-label">No Agenda</label>
+                    </div>
+                    <div class="card-white">
+                        <label for="seri">No Seri</label>
+                        <div class="separator"></div>
+                        <input type="text" id="seri">
+                    </div>
+                    <div class="card-white">
+                        <label for="diterima">Diterima</label>
+                        <div class="separator"></div>
+                        <input type="text" id="diterima">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card-blue">
+                        <label for="tgl_surat" class="form-label">Status Surat</label>
+                    </div>
+                    <div class="card-white">
+                        <label for="status">Status</label>
+                        <div class="separator"></div>
+                        <button class="status">Diproses</button>
+                    </div>
+                    <div class="card-white">
+                        <label for="tanggal">Tanggal</label>
+                        <div class="separator"></div>
+                        <input type="text" id="tanggal">
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-4" style="gap: 20px;">
+                <div class="col">
+                    <div class="card-blue">
+                        <label for="tgl_surat" class="form-label">
+                            <img src="/img/memo-admin/info.png" alt="info surat">Informasi Detail Surat
+                        </label>
+                    </div>
+                    <div class="card-white">
+                        <label for="nomor">No Dokumen</label>
+                        <div class="separator"></div>
+                        <input type="text" id="nomor">
+                    </div>
+                    <div class="card-white">
+                        <label for="divisi">Divisi</label>
+                        <div class="separator"></div>
+                        <input type="text" id="seri">
+                    </div>
+                    <div class="card-white">
+                        <label for="perihal">Perihal</label>
+                        <div class="separator"></div>
+                        <input type="text" id="perihal">
+                    </div>
+                    <div class="card-white">
+                        <label for="tgl">Tanggal Surat</label>
+                        <div class="separator"></div>
+                        <input type="text" id="tgl">
+                    </div>
+                    <div class="card-white">
+                        <label for="lampiran">Lampiran</label>
+                        <div class="separator"></div>
+                        <input type="text" id="kepada">
+                    </div>
+                    <div class="card-white">
+                        <label for="file">File</label>
+                        <div class="separator"></div>
+                        <button class="btn-file"><img src="/img/mata.png" alt="view"><a href="#">Lihat</a></button>
+                        <button class="down btn-file"><img src="/img/download.png" alt="down"><a href="#">Unduh</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-4" style="gap: 20px;">
+                <div class="col">
+                    <div class="card-blue">
+                        <label for="tgl_surat" class="form-label">
+                            <img src="/img/memo-admin/info.png" alt="info surat">Informasi Pimpinan
+                        </label>
+                    </div>
+                    <div class="card-white">
+                        <label for="status">Pengesahan</label>
+                        <div class="separator"></div>
+                        <button class="status">Diproses</button>
+                    </div>
+                    <div class="card-white">
+                        <label for="tindakan">Tindakan Selanjutnya</label>
+                        <div class="separator"></div>
+                        <input type="text" id="tindakan">
+                    </div>
+                    <div class="card-white">
+                        <label for="catatan">Catatan</label>
+                        <div class="separator"></div>
+                        <input type="text" id="catatan">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <button type="button" class="btn back" id="backBtn">Kembali</button>
+            <button type="button" class="btn submit" id="submitBtn" data-bs-toggle="modal" data-bs-target="#submit">Kirim</button>
+        </div>
+    </div>
+</body>
+</html>
