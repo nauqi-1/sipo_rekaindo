78d6b0a2eef9acc82093f4bc1048e10ce7985473
prihandiva <prihandiva1711@gmail.com>
2025-07-23 07:56:01 +0700

    fix(dashboard):groupby id_document

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,34 +16,46 @@ class DashboardController extends Controller
     public function index()
     {
         // Menghitung jumlah memo yang sudah dibuat
-        
+
         $userDivisiId = auth()->user()->divisi_id_divisi; // Ambil divisi user yang login
         $kirimDocuments = Kirim_Document::where('id_penerima', Auth::user()->id)->get();
 
         $jumlahMemo = Kirim_Document::where('jenis_document', 'memo')
-                    ->where(function($query) {
-                        $query->where('id_pengirim', Auth::user()->id)
-                            ->orWhere('id_penerima', Auth::user()->id);
-                    })
-                    ->count();
+            ->where(function ($query) {
+                $query->where('id_pengirim', Auth::user()->id)
+                    ->orWhere('id_penerima', Auth::user()->id);
+            })
+            ->select('id_document')
+            ->groupBy('id_document')
+            ->get()
+            ->count();
+
         $jumlahRisalah = Kirim_Document::where('jenis_document', 'risalah')
-                    ->where(function($query) {
-                        $query->where('id_pengirim', Auth::user()->id)
-                            ->orWhere('id_penerima', Auth::user()->id);
-                    })
-                    ->count();
+            ->where(function ($query) {
+                $query->where('id_pengirim', Auth::user()->id)
+                    ->orWhere('id_penerima', Auth::user()->id);
+            })
+            ->select('id_document')
+            ->groupBy('id_document')
+            ->get()
+            ->count();
+
+
         $jumlahUndangan = Kirim_Document::where('jenis_document', 'undangan')
-                    ->where(function($query) {
-                        $query->where('id_pengirim', Auth::user()->id)
-                            ->orWhere('id_penerima', Auth::user()->id);
-                    })
-                    ->count();
+            ->where(function ($query) {
+                $query->where('id_pengirim', Auth::user()->id)
+                    ->orWhere('id_penerima', Auth::user()->id);
+            })
+            ->select('id_document')
+            ->groupBy('id_document')
+            ->get()
+            ->count();
 
         $Memo = Memo::all()->count();
         $Undangan = Undangan::all()->count();
         $Risalah = Risalah::all()->count();
 
         // Kirim data ke view
-        return view(Auth::user()->role->nm_role.'.dashboard', compact('jumlahMemo','jumlahRisalah','jumlahUndangan','Memo','Undangan','Risalah'));  
+        return view(Auth::user()->role->nm_role . '.dashboard', compact('jumlahMemo', 'jumlahRisalah', 'jumlahUndangan', 'Memo', 'Undangan', 'Risalah'));
     }
-}   
+}
