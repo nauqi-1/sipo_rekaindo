a586aa3fa753f055e8d82910d19c2d32cb8eae23
nauqi-1 <nauqi2208@gmail.com>
2025-07-28 09:22:29 +0700

    fix(memo): edit memo utk superadmin

--- resources/views/admin/memo/edit-memo.blade.php
+++ resources/views/admin/memo/edit-memo.blade.php
@@ -9,13 +9,14 @@
     <script src="https://cdn.jsdelivr.net/npm/summernote/dist/summernote-lite.min.js"></script>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/admin/edit-memo.css') }}">
 
     <!--DEPENDENCY UNTUK JSTREE-->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.12/themes/default/style.min.css" rel="stylesheet">    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.12/jstree.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+
 </head>
 <body>
     <div class="pc-container">
@@ -75,6 +76,7 @@
                     <div style="width: 95%;">
                     <label style="font-size: small;" for="kepada" class="form-label">
                         <img src="/img/undangan/kepada.png" alt="kepada" style="margin-right: 5px;">Kepada <span class="text-danger">*</span>
+                         <span class="text-danger" style="font-size: x-small;">Cukup pilih tujuan tanpa memilih tingkat dibawahnya.</span>
                     </label>
                     
                     <div id="orgTreeError" class="form-control text-danger" style="display:none;"></div>
